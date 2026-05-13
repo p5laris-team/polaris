f8f2c52a3df936d9ea3e05255b40bf81036201c6
thetana <zxsa135@naver.com>
2026-05-13 19:53:25 +0900

    First (#4)
    * first commit
    
    * feat: 각 모듈 업로드
    
    ---------
    
    Co-authored-by: zxsa135 <zxsa13577@gmail.com>

---
+++ user/src/test/java/p5laris/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package p5laris.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
