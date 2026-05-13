f0be63902a11f9ff7f912248e9ac5a2ef5bafe48
thetana <zxsa135@naver.com>
2026-05-13 19:42:17 +0900

    feat: 각 모듈 업로드

---
+++ ai/src/test/java/p5laris/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package p5laris.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ character/src/main/java/p5laris/character/CharacterApplication.java
@@ -0,0 +1,13 @@
+package p5laris.character;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharacterApplication.class, args);
+    }
+
+}
