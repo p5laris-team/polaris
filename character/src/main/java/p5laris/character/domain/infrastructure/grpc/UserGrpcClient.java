2c832f28d16a24aa769996099013ef0e46d57310
thetana <zxsa135@naver.com>
2026-05-14 10:10:11 +0900

    First (#6)
    * first commit
    
    * feat: 각 모듈 업로드
    
    * feat: 통신 잘 되나 확인
    
    * feat: 패키지 구조 올리기 위해 빈파일 올림
    
    ---------
    
    Co-authored-by: zxsa135 <zxsa13577@gmail.com>

---
+++ character/src/main/java/p5laris/character/domain/infrastructure/grpc/UserGrpcClient.java
@@ -0,0 +1,24 @@
+package p5laris.character.domain.infrastructure.grpc;
+
+import com.p5laris.proto.user.v1.PingPongRequest;
+import com.p5laris.proto.user.v1.PingPongResponse;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+import com.p5laris.proto.user.v1.UserServiceGrpc;
+
+@Service
+public class UserGrpcClient {
+
+    @GrpcClient("user")
+    private UserServiceGrpc.UserServiceBlockingStub userStub;
+
+    public String checkUser(String value) {
+        PingPongResponse response = userStub.pingPong(
+                PingPongRequest.newBuilder()
+                        .setMessage(value)
+                        .build()
+        );
+
+        return response.getMessage();
+    }
+}
