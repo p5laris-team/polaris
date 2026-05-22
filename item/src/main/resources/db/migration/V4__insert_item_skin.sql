7b5715400910a173620ef1c36d17f91be6f5f582
sohyun kim <sohyun050282@gmail.com>
2026-05-22 12:25:12 +0900

    feat: item 스킨 추가

---
+++ item/src/main/resources/db/migration/V4__insert_item_skin.sql
@@ -0,0 +1,2 @@
+INSERT INTO items (name, description, item_type, price, effect, effect_type, image_url, active) VALUES
+('푸른 새벽 스킨', '푸른 새벽처럼 신비로운 스킨입니다.', 'SKIN', 100, NULL, NULL, 'https://cdn.polaris.app/items/skin-blue-dawn.png', TRUE);
