b0e08cbe11ea118097ae3eb49a1bdea3aab2956f
thetana <zxsa135@naver.com>
2026-05-23 23:22:45 +0900

    fix: @EnableJpaAuditing 안넣음 베이스 엔티티 동작 안함

--- notification/src/main/java/p5laris/notification/NotificationApplication.java
+++ notification/src/main/java/p5laris/notification/NotificationApplication.java
@@ -2,7 +2,11 @@ package p5laris.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
+@EnableJpaAuditing
 @SpringBootApplication
 public class NotificationApplication {
 
