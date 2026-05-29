09f87f2a5aad7be0fbf497e795714813ca1ddbc4
HyunjiPark <intheji3830@gmail.com>
2026-05-29 20:35:30 +0900

    feat: 사용자 기억을 미션 생성 context에 포함

--- mission/src/main/java/p5laris/mission/domain/domain/repository/UserMemoryRepository.java
+++ mission/src/main/java/p5laris/mission/domain/domain/repository/UserMemoryRepository.java
@@ -1,13 +1,18 @@
 package p5laris.mission.domain.domain.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import p5laris.mission.domain.domain.entity.UserMemory;
 import p5laris.mission.domain.domain.enums.UserMemorySourceType;
 import p5laris.mission.domain.domain.enums.UserMemoryType;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserMemoryRepository extends JpaRepository<UserMemory, Long> {
+
+    // AI context에는 최근 기억 일부만 넣어 토큰 비용과 조회 비용을 제한한다.
+    List<UserMemory> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
 
     Optional<UserMemory> findBySourceTypeAndSourceIdAndMemoryType(
             UserMemorySourceType sourceType,
