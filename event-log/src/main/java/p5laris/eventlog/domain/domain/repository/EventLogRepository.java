29b6a683d206251abbe470d61752e159f46ce78e
sohyun kim <sohyun050282@gmail.com>
2026-06-02 09:30:51 +0900

    feat: 운영 리뷰 1-4 작업

--- event-log/src/main/java/p5laris/eventlog/domain/domain/repository/EventLogRepository.java
+++ event-log/src/main/java/p5laris/eventlog/domain/domain/repository/EventLogRepository.java
@@ -2,6 +2,9 @@ package p5laris.eventlog.domain.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import p5laris.eventlog.domain.domain.entity.EventLog;
+import java.util.UUID;
 
 public interface EventLogRepository extends JpaRepository<EventLog, Long> {
+    boolean existsByEventId(UUID eventId);
 }
+
