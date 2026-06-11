08dcfd059d253c03e1854b03ea393765ecf63bb2
sohyun kim <sohyun050282@gmail.com>
2026-06-11 17:58:16 +0900

    feat: 공통 모듈 common 작업

--- common/src/main/java/p5laris/common/entity/BaseEntity.java
+++ common/src/main/java/p5laris/common/entity/BaseEntity.java
@@ -10,6 +10,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+/**
+ * 데이터베이스의 등록 시간(created_at)과 수정 시간(updated_at)을 자동으로 기록하고 관리하는
+ * JPA Auditing 용 공통 추상 엔티티 클래스입니다.
+ * 
+ * 여러 모듈의 엔티티 클래스가 상속받아 일관되게 감시(Auditing)를 수행하도록 단일화되었습니다.
+ */
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
--- common/src/main/java/p5laris/common/tracing/GrpcTraceConfig.java
+++ common/src/main/java/p5laris/common/tracing/GrpcTraceConfig.java
@@ -9,6 +9,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import p5laris.common.deadline.GrpcDeadlineClientInterceptor;
 
+/**
+ * gRPC 전역 인터셉터들을 스프링 부트 자동 설정(AutoConfiguration) 메커니즘을 통해 등록해주는 설정 클래스입니다.
+ * 
+ * `@ConditionalOnClass`를 활용하여 각 모듈의 클래스패스 유무에 맞춰 서버용/클라이언트용 추적 인터셉터와
+ * 데드라인 제어 인터셉터가 동적으로 자동 주입되도록 구현하여, 모듈별 복사-붙여넣기 설정 중복을 최소화합니다.
+ */
 @Configuration
 public class GrpcTraceConfig {
 
